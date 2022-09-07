4d2a347efd289596f3f29ce4f39267c36b25cbb1
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-09-08 00:34:14 +0200

    08.09.2022

--- src/test/java/tests/KnowledgeWebseite.java
+++ src/test/java/tests/KnowledgeWebseite.java
@@ -3,6 +3,7 @@ package tests;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -48,11 +49,40 @@ public class KnowledgeWebseite extends TestBase {
         emailButton.sendKeys(faker.internet().emailAddress());
         Thread.sleep(2000);
         WebElement nachrrichtButton = driver.findElement(By.xpath("(//*[@class='messageinput'])[1]"));
-        nachrrichtButton.sendKeys("calm down only a test for bewerbung:)");
+        nachrrichtButton.sendKeys("calm down only a test for bewerbung:)please dont answer");
         Thread.sleep(2000);
-        WebElement sendentButton = driver.findElement(By.xpath("(//*[@class='messageinput'])[1]"));
-        sendentButton.sendKeys("calm down only a test for bewerbung:) please dont antwort");
+        WebElement sendentButton = driver.findElement(By.xpath("(//*[@class='messagebutton'])[1]"));
+        sendentButton.click();
         Thread.sleep(2000);
+
+        WebElement messagegesendet=driver.findElement(By.xpath("//*[@class='message message-Information']"));
+        Assert.assertTrue(messagegesendet.isDisplayed());
+
+    }
+
+    @Test
+    public void loginTest() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement cookies = driver.findElement(By.xpath("//*[text()='Einverstanden']"));
+        cookies.click();
+        Thread.sleep(2000);
+        WebElement loginbutton = driver.findElement(By.xpath("(//*[text()='Login'])[1]"));
+        loginbutton.click();
+        Thread.sleep(2000);
+        WebElement usernamesbutton = driver.findElement(By.xpath("(//*[@class='logininput'])[1]"));
+        usernamesbutton.sendKeys(faker.name().username());
+        Thread.sleep(2000);
+        WebElement passwordsbutton = driver.findElement(By.xpath("(//*[@class='logininput'])[2]"));
+        passwordsbutton.sendKeys(faker.internet().password());
+        Thread.sleep(2000);
+        WebElement absendenButton = driver.findElement(By.xpath("//*[text()='Absenden']"));
+        absendenButton.submit();
+        Thread.sleep(2000);
+        WebElement loginControl = driver.findElement(By.xpath("//*[text()='Login fehlgeschlagen!']"));
+        Assert.assertTrue(loginControl.isDisplayed());
+    }
+    @Test
+    public void passwordVergessenTest(){
 
     }
 }
